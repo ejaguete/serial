9ff7897f1a9469bd3f181dfb78ab93269aba1323
ejaguete <erica.aguete@gmail.com>
2017-11-15 13:30:28 -0700

    v9
    - added client socket to Sender
    -added server socket to receiver

--- serial/src/serial/Receiver.java
+++ serial/src/serial/Receiver.java
@@ -1,5 +1,46 @@
 package serial;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+import org.jdom2.input.SAXBuilder;
+
 public class Receiver {
+	
+	public static void main(String[] args) throws IOException {
+		ServerSocket socket = new ServerSocket(Sender.port);
+		System.out.println("Server 'receiver' initialized");
+		int ch;
+		
+		while(true) {
+			Socket connection = socket.accept();
+			
+			BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
+			InputStreamReader ir = new InputStreamReader(is);
+			StringBuffer buf = new StringBuffer();
+			
+			// read input stream 
+			while((ch=ir.read())!=13)
+				buf.append((char) ch);
+			
+			System.out.println(buf);
+			
+			String resp = "Server 'receiver' responded" + (char) 13;
+			BufferedOutputStream os = new BufferedOutputStream(connection.getOutputStream());
+			OutputStreamWriter ow = new OutputStreamWriter(os, "US-ASCII");
+			ow.write(resp);
+			ow.flush();
+		}
+		
+		
+
+	}
 
 }
--- serial/src/serial/Sender.java
+++ serial/src/serial/Sender.java
@@ -1,17 +1,31 @@
 package serial;
 
 import java.awt.EventQueue;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Sender {
+	
+	public static String host = "localhost";
+	public static int port = 9000;
 	
 	public static ArrayList<Object> objects = new ArrayList<Object>();
 	
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		// create objects & store in array
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -23,6 +37,32 @@ public class Sender {
 				}
 			}
 		});
+		
+		System.out.println("Client 'sender' initialized");
+		// request socket connection
+		try {
+			InetAddress address = InetAddress.getByName(host);
+			Socket connection = new Socket(address, port);
+			BufferedOutputStream os = new BufferedOutputStream(connection.getOutputStream());
+			OutputStreamWriter ow = new OutputStreamWriter(os, "US-ASCII");
+			String msg = "Calling server" + (char) 13;
+			ow.write(msg);
+			ow.flush();
+
+			StringBuffer buf = new StringBuffer();
+			// read from socket
+			BufferedInputStream is = new BufferedInputStream(connection.getInputStream());
+			InputStreamReader ir = new InputStreamReader(is, "US-ASCII");
+			
+			int c;
+			while((c=ir.read())!=13)
+				buf.append((char) c);
+			
+			connection.close();
+			System.out.println(buf);
+		} finally {}
+		
+		
 	}
 
 }
